747f19a1bc1dd43cf6ab7d68fa141dbb04c8d668
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-06-06 13:01:21 -0500

    add bootstrap, work in banner and navbar

---
+++ client/src/NavBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NavBar = () => {
+	return (
+		<p>Navbar goes here</p>
+		);
+}
+
+export default NavBar
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
