7c00b529cba59eb72f55f20bc5c35fd9b1402660
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-06-13 15:47:33 -0500

    build out Videos component

--- client/src/containers/Videos.js
+++ client/src/containers/Videos.js
@@ -3,3 +3,38 @@ import { bindActionCreators } from 'redux';
 import * as actions from '../actions/videoActions';
 import { connect } from 'react-redux';
 import VidsList from '../components/VidsList';
+
+class Videos extends React.Component {
+
+	componentDidMount() {
+		if (this.props.vidTiles.length === 0) {
+			this.props.actions.fetchVids(); 
+		}
+	}
+
+	render() {
+		return (
+			<div className="justify-content-center">
+				<h1><strong>Videos</strong></h1>
+				<h3>You can find a couple music videos and a live video below. 
+				For more videos and playlists, check out the <a href="https://www.youtube.com/user/doctorsomething">Dr. Something YouTube page</a>.</h3>
+				<VidsList vidTiles={this.props.vidTiles} loading={this.props.loading} />
+			</div>
+		)
+	}
+}
+
+const mapStateToProps = (state) => {
+	return {
+		vidTiles: state.videos.vids 
+		loading: state.videos.loading
+	}
+}	
+
+const mapDispatchToProps = (dispatch) => {
+	return {actions: bindActionCreators(actions, dispatch)}
+}
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Videos)
