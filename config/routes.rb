3908c70fbee3bbde6915ea5c17d06c62e8b2317a
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-06-13 16:49:03 -0500

    addd index route for video on API

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/songs', to: 'songs#index'
   get '/illustrations', to: 'illustrations#index'
+  get '/videos', to: 'videos#index'
 end
