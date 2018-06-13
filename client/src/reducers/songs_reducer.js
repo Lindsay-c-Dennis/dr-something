f60c9d03e9ecaca64f93fea2625a9b001ef94893
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-06-13 10:51:44 -0500

    build out reducer to respond to song fetch

--- client/src/reducers/songs_reducer.js
+++ client/src/reducers/songs_reducer.js
@@ -1,10 +1,15 @@
-export default (state= {loading: false, songs: []}, action) => {
+const songsReducer = (state= {loading: false, tiles: [], error: ''}, action={}) => {
 	switch (action.type) {
 		case 'LOADING_SONGS':
-			return [...state, {loading: true}]
-		case 'FETCH_SONGS':
-			return {loading: false, songs: action.payload}
+			return {...state, loading: true}
+		case 'FETCH_SONGS_SUCCESS':
+			//debugger
+			return {loading: false, tiles: action.payload}
+		case 'FETCH_SONGS_FAILED':
+			return {...state, error: action.payload, loading: false}
 		default:
 			return state;		
 	}
 }
+
+export default songsReducer;
