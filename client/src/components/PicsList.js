ab3bc7bf08e496647e5c880fba6da34d542a1878
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-06-13 13:46:53 -0500

    flesh out picslist component

--- client/src/components/PicsList.js
+++ client/src/components/PicsList.js
@@ -1,16 +1,20 @@
 import React from 'react';
 
 const PicsList = (props) => {
-	const showPics = props.picsTiles.map(picTile => {
+	const showPics = props.picTiles.map(picTile => {
 		return (
 			<div className="justify-content-center">
 				<img src={picTile.image_url} alt={picTile.artist} width="75%" />
-				<p><a href={picTile.artist_url}>{picTile.artist} performing at {picTile.venue} on {picTile.date}</p>
+				<h3><a href={picTile.artist_url}>{picTile.artist}</a> performing at {picTile.venue} on {picTile.date}</h3>
 			</div>
 		)
 	})
 
-	return ({ showPics })
+	return (
+		<div>
+			{ showPics }
+		</div>
+	)
 }
 
 export default PicsList;
