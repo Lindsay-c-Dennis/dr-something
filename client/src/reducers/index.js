5512b68d41e0bc205c8f99419871a695605f46c4
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-06-13 16:51:30 -0500

    add video reducer to root reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import songsReducer from './songs_reducer';
 import illustrationsReducer from './illustrations_reducer';
+import videosReducer from './videos_reducer';
 
 const rootReducer = combineReducers({
 	songs: songsReducer,
-	illustrations: illustrationsReducer
+	illustrations: illustrationsReducer,
+	videos: videosReducer
 });
 
 export default rootReducer;
