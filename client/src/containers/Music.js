38322153fe1422046f12457d7930d5d42f8d8a16
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-06-13 10:51:00 -0500

    modify container to fetch songs from API

--- client/src/containers/Music.js
+++ client/src/containers/Music.js
@@ -1,32 +1,41 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import MusicCard from '../components/MusicCard'
 import { bindActionCreators } from 'redux';
 import * as actions from '../actions/songActions.js'
+import SongsList from '../components/SongsList'
 
 class Music extends React.Component {
 
 	componentDidMount() {
-    	if (this.props.songTiles.length === 0) {
-      		this.props.actions.fetchSongs();
+        if (this.props.songTiles.length === 0) {
+        this.props.actions.fetchSongs();
     	}
-  	}
+    }
 
 	render() {
-		const { songs } = this.props
+		//debugger
 		return (
 			<div>	
 				<div className="row justify-content-center">
 					<h1>Music</h1>
 				</div>
-				{songs.map(song => <MusicCard key={song.id} song={song} />)}	
+				<SongsList songTiles={this.props.songTiles} loading={this.props.loading} />
+					
 			</div>		
 			);
 	}
 }
 
 function mapStateToProps(state) {
-	return {songs: state.songs}
+	//debugger
+	return {
+		songTiles: state.songs.tiles,
+		loading: state.songs.loading
+	}
 }
 
-export default connect(mapStateToProps)(Music);
+const mapDispatchToProps = (dispatch) => {
+	return {actions: bindActionCreators(actions, dispatch)}
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Music);
