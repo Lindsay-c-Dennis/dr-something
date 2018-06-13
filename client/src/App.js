c81f2d1ce412441cb4d430ebd1d20fc51541e0d0
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-06-13 10:54:29 -0500

    seed songs, delete unnecessary component, build skeleton components

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 import NavBar from './NavBar';
 import Home from './components/Home';
 import UpcomingShows from './components/UpcomingShows';
@@ -9,9 +10,9 @@ import Music from './containers/Music';
 import Videos from './components/Videos';
 import SoundsIllustrated from './components/SoundsIllustrated';
 
+
 class App extends Component {
   
- 
 
   render() {
     return (
@@ -22,12 +23,12 @@ class App extends Component {
         <Router>
           <div>
             <NavBar />
-            <Route exact path="/" component={Home} />
-            <Route exact path="/upcomingshows" component={UpcomingShows} />
-            <Route exact path="/news" component={News} />
-            <Route exact path="/music" component={Music} />
-            <Route exact path="/videos" component={Videos} />
-            <Route exact path="/soundsillustrated" component={SoundsIllustrated} />
+              <Route exact path="/" component={Home} />
+              <Route exact path="/upcomingshows" component={UpcomingShows} />
+              <Route exact path="/news" component={News} />
+              <Route exact path="/music" component={Music} />
+              <Route exact path="/videos" component={Videos} />
+              <Route exact path="/soundsillustrated" component={SoundsIllustrated} />
           </div>
         </Router>    
       </div>
@@ -35,4 +36,8 @@ class App extends Component {
   }
 }
 
+
+
 export default App;
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,4 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Song.create(title: "Companion (Instrumental)", streamUrl: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/428033055&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true")
 Song.create(title: "Rustic Machinery (Instrumental)", streamUrl: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/428032836&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true")
-Song.create(title: "Red Rover (Instrumental)", streamUrl: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/428032836&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true)
+Song.create(title: "Red Rover (Instrumental)", streamUrl: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/428032836&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true")
