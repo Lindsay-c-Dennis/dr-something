5db6c2b090c97c3b9226b0f2fd92ed0724b7e358
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-06-13 13:45:14 -0500

    add illustrations reducer to root reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import songsReducer from './songs_reducer';
+import illustrationsReducer from './illustrations_reducer';
 
 const rootReducer = combineReducers({
-	songs: songsReducer
+	songs: songsReducer,
+	illustrations: illustrationsReducer
 });
 
 export default rootReducer;
