5958c630dfd8a32dc04b8ad97a6bf3c7615b7601
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-06-14 15:03:25 -0500

    add reviews reducer to root reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import songsReducer from './songs_reducer';
 import illustrationsReducer from './illustrations_reducer';
 import videosReducer from './videos_reducer';
+import reviewsReducer from './reviews_reducer';
 
 const rootReducer = combineReducers({
 	songs: songsReducer,
