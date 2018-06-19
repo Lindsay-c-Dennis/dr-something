3a13f20c4845619f9b3a82aa8d2ba757e0fd5623
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-06-19 17:05:57 -0500

    add facebook and instagram links, add show poster

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -10,6 +10,8 @@ const Home = () => {
 			 <a href="https://open.spotify.com/album/4mxYhIpidurvy3cNG1w9DG?play=true&utm_source=open.spotify.com&utm_medium=open"> Spotify</a>, 
 			 <a href="http://wordpress.redirectingat.com/?id=725X1342&site=drsomething.wordpress.com&xs=1&isjs=1&url=https%3A%2F%2Fitunes.apple.com%2Fus%2Falbum%2Frustic-machinery-ep%2Fid1225087632%3Fapp%3Ditunes%26ign-mpt%3Duo%253D4&xguid=6c8343b44d0ebe5dfad5778725d643d3&xuuid=8289a5a09280df9883033a99c72783ba&xsessid=6687c44d35d0e062510dfcf972022965&xcreo=0&xed=0&sref=https%3A%2F%2Fdrsomething.com%2F&pref=https%3A%2F%2Fdrsomething.com%2Fmusic%2F&xtz=300&abp=1&jv=13.6.1-2&bv=2.5.1"> Apple Music </a> 
 			 and <a href="http://wordpress.redirectingat.com/?id=725X1342&site=drsomething.wordpress.com&xs=1&isjs=1&url=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fproduct%2FB06Y5G3X9H&xguid=6c8343b44d0ebe5dfad5778725d643d3&xuuid=8289a5a09280df9883033a99c72783ba&xsessid=6687c44d35d0e062510dfcf972022965&xcreo=0&xed=0&sref=https%3A%2F%2Fdrsomething.com%2F&pref=https%3A%2F%2Fdrsomething.com%2Fmusic%2F&xtz=300&abp=1&jv=13.6.1-2&bv=2.5.1">Amazon</a>.</h3>  
+			<h3>Follow me on <a href="https://www.facebook.com/drsomethingmusic/">Facebook</a> and <a href="https://www.instagram.com/drsomethingmusic/">Instagram</a>!</h3> 
+			<img src="https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/35344810_1828233583889466_5943771121714200576_n.jpg?_nc_cat=0&oh=b7bfd1baeded672125b7c47ec2e58473&oe=5BC13A89" alt="show June 28" />
 		</div>
 		);
 }
